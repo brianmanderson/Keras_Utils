03d64c38e878f766a418cc0c3055900dcb05097f
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-01-08 10:22:08 -0600

    added an earlystopping, pull latest weights if there is a massive drop

--- Keras_Utilities.py
+++ Keras_Utilities.py
@@ -652,6 +652,126 @@ class Save_History_Class_3D_new(Callback):
         return logs
 
 
+class EarlyStopping_BMA(Callback):
+    """Stop training when a monitored quantity has stopped improving.
+
+    # Arguments
+        monitor: quantity to be monitored.
+        min_delta: minimum change in the monitored quantity
+            to qualify as an improvement, i.e. an absolute
+            change of less than min_delta, will count as no
+            improvement.
+        max_delta: max decrease in the monitored quantity to qualify as needing to restart
+        patience: number of epochs with no improvement
+            after which training will be stopped.
+        verbose: verbosity mode.
+        mode: one of {auto, min, max}. In `min` mode,
+            training will stop when the quantity
+            monitored has stopped decreasing; in `max`
+            mode it will stop when the quantity
+            monitored has stopped increasing; in `auto`
+            mode, the direction is automatically inferred
+            from the name of the monitored quantity.
+        baseline: Baseline value for the monitored quantity to reach.
+            Training will stop if the model doesn't show improvement
+            over the baseline.
+        restore_best_weights: whether to restore model weights from
+            the epoch with the best value of the monitored quantity.
+            If False, the model weights obtained at the last step of
+            training are used.
+    """
+
+    def __init__(self,
+                 monitor='val_loss',
+                 min_delta=0,
+                 max_delta=0,
+                 patience=0,
+                 verbose=0,
+                 mode='auto',
+                 baseline=None,
+                 restore_best_weights=False):
+        super(EarlyStopping_BMA, self).__init__()
+
+        self.monitor = monitor
+        self.baseline = baseline
+        self.patience = patience
+        self.verbose = verbose
+        self.min_delta = min_delta
+        self.max_delta = max_delta
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.restore_best_weights = restore_best_weights
+        self.best_weights = None
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('EarlyStopping mode %s is unknown, '
+                          'fallback to auto mode.' % mode,
+                          RuntimeWarning)
+            mode = 'auto'
+        if max_delta == 0:
+            self.dif = lambda best, current: np.inf
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.dif = lambda best,current: best - current
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.dif = lambda best, current: current - best
+        else:
+            if 'acc' in self.monitor:
+                self.monitor_op = np.greater
+            else:
+                self.monitor_op = np.less
+        if self.monitor_op == np.greater:
+            self.min_delta *= 1
+        else:
+            self.min_delta *= -1
+
+    def on_train_begin(self, logs=None):
+        # Allow instances to be re-used
+        self.wait = 0
+        self.stopped_epoch = 0
+        if self.baseline is not None:
+            self.best = self.baseline
+        else:
+            self.best = np.Inf if self.monitor_op == np.less else -np.Inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        current = self.get_monitor_value(logs)
+        if current is None:
+            return
+
+        if self.monitor_op(current - self.min_delta, self.best):
+            self.best = current
+            self.wait = 0
+            if self.restore_best_weights:
+                self.best_weights = self.model.get_weights()
+        else:
+            self.wait += 1
+            if self.wait >= self.patience or self.dif(self.best,current) < self.max_delta:
+                self.wait = 0
+                self.stopped_epoch = epoch
+                self.model.stop_training = False
+                if self.restore_best_weights:
+                    if self.verbose > 0:
+                        print('Restoring model weights from the end of '
+                              'the best epoch')
+                    self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0 and self.verbose > 0:
+            print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+
+    def get_monitor_value(self, logs):
+        monitor_value = logs.get(self.monitor)
+        if monitor_value is None:
+            warnings.warn(
+                'Early stopping conditioned on metric `%s` '
+                'which is not available. Available metrics are: %s' %
+                (self.monitor, ','.join(list(logs.keys()))), RuntimeWarning
+            )
+        return monitor_value
+
+
 class ModelCheckpoint_new(ModelCheckpoint):
 
     def __init__(self, filepath, monitor='val_loss', verbose=0,
